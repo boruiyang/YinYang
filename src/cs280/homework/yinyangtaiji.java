0fc44c94ce7f690c14327f28e351db16ea159a35
YANG_BORU <boruiyang@hotmai.com>
2016-04-21 23:19:39 -0400

    Added new effect

--- src/cs280/homework/yinyangtaiji.java
+++ src/cs280/homework/yinyangtaiji.java
@@ -34,6 +34,11 @@ public class yinyangtaiji extends Application {
 
     private double x_coordinate = 0;
     private double y_coordinate = 0;
+
+    public static void main( String[] args ) {
+        launch();
+    }
+
     public void start(Stage stage) {
         Group root = new Group();
         root.setOnMousePressed(new EventHandler<MouseEvent>() {
@@ -50,21 +55,32 @@ public class yinyangtaiji extends Application {
                 stage.setY(event.getScreenY() - y_coordinate);
             }
         });
-        Canvas canvas = new Canvas(530, 500);
+        Canvas canvas = new Canvas( 630, 600 );
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        Rectangle colors = new Rectangle(530, 500,
-                new LinearGradient(0f, 1f, 1f, 0f, true, CycleMethod.NO_CYCLE, new Stop[]{
-                        new Stop(0, Color.web( "white",0.9 )),
+        Rectangle colors = new Rectangle( 550, 520,
+                new LinearGradient( 0f, 1f, 1f, 0f, true, CycleMethod.NO_CYCLE, new Stop( 0, Color.web( "white", 0.9 ) ),
                         new Stop(0.14, Color.web( "black",0.9 )),
                         new Stop(0.28,Color.web( "white",0.9 )),
                         new Stop(0.43, Color.web( "black",0.9 )),
                         new Stop(0.57, Color.web( "white",0.9 )),
                         new Stop(0.71, Color.web( "black",0.9 )),
                         new Stop(0.85, Color.web( "white",0.9 )),
-                        new Stop(1, Color.web( "black",0.9 )),
-                }));
+                        new Stop( 1, Color.web( "black", 0.9 ) ) ) );
+        colors.setX( 37 );
+        colors.setY( 38 );
 
-        Circle circle = new Circle( 265,250,165 );
+        Rectangle blackRec = new Rectangle( canvas.getWidth(), canvas.getHeight() );
+        blackRec.setFill( Color.BLACK );
+        blackRec.setEffect( new GaussianBlur() );
+
+        Rectangle whiteRec = new Rectangle( 580, 550 );
+        whiteRec.setFill( Color.WHITE );
+        whiteRec.setX( 23 );
+        whiteRec.setY( 24 );
+        whiteRec.setEffect( new GaussianBlur() );
+
+
+        Circle circle = new Circle( 320, 305, 165 );
         circle.setFill( Color.web( "white",0.8 ) );
         circle.setStrokeType( StrokeType.OUTSIDE );
         circle.setStroke(Color.web( "black",0.8 ));
@@ -72,87 +88,90 @@ public class yinyangtaiji extends Application {
         circle.setEffect( new GaussianBlur() );
 
         Text yin = new Text( "阴" );
-        yin.setX( 5 );
-        yin.setY( 100 );
+        yin.setX( 50 );
+        yin.setY( 135 );
         yin.setFont( Font.font("汉仪雪君体繁", FontWeight.BOLD,80 ));
         yin.setFill( WHITE );
         yin.setStrokeType( StrokeType.OUTSIDE );
         yin.setStroke(Color.web( "black",0.70 ));
         yin.setStrokeWidth(5);
         Text yang = new Text( "阳" );
-        yang.setX( 5 );
-        yang.setY( 200 );
+        yang.setX( 50 );
+        yang.setY( 255 );
         yang.setFont( Font.font("汉仪雪君体繁", FontWeight.BOLD,80 ));
         yang.setFill( WHITE );
         yang.setStrokeType( StrokeType.OUTSIDE );
         yang.setStroke(Color.web( "black",0.70 ));
         yang.setStrokeWidth(5);
         Text liang = new Text( "两" );
-        liang.setX( 5 );
-        liang.setY( 300 );
+        liang.setX( 50 );
+        liang.setY( 355 );
         liang.setFont( Font.font("汉仪雪君体繁", FontWeight.BOLD,80 ));
         liang.setFill( WHITE );
         liang.setStrokeType( StrokeType.OUTSIDE );
         liang.setStroke(Color.web( "black",0.70 ));
         liang.setStrokeWidth(5);
         Text yi = new Text( "仪" );
-        yi.setX( 5 );
-        yi.setY( 400 );
+        yi.setX( 50 );
+        yi.setY( 455 );
         yi.setFont( Font.font("汉仪雪君体繁", FontWeight.BOLD,80 ));
         yi.setFill( WHITE );
         yi.setStrokeType( StrokeType.OUTSIDE );
         yi.setStroke(Color.web( "black",0.70 ));
         yi.setStrokeWidth(5);
         Text dao = new Text( "道" );
-        dao.setX( 440 );
-        dao.setY( 100 );
+        dao.setX( 495 );
+        dao.setY( 135 );
         dao.setFont( Font.font("汉仪雪君体繁", FontWeight.BOLD,80 ));
         dao.setFill( WHITE );
         dao.setStrokeType( StrokeType.OUTSIDE );
         dao.setStroke(Color.web( "black",0.70 ));
         dao.setStrokeWidth(5);
         Text wu = new Text( "无" );
-        wu.setX( 440 );
-        wu.setY( 200 );
+        wu.setX( 495 );
+        wu.setY( 255 );
         wu.setFont( Font.font("汉仪雪君体繁", FontWeight.BOLD,80 ));
         wu.setFill( WHITE );
         wu.setStrokeType( StrokeType.OUTSIDE );
         wu.setStroke(Color.web( "black",0.70 ));
         wu.setStrokeWidth(5);
         Text ji = new Text( "极" );
-        ji.setX( 440 );
-        ji.setY( 300 );
+        ji.setX( 495 );
+        ji.setY( 355 );
         ji.setFont( Font.font("汉仪雪君体繁", FontWeight.BOLD,80 ));
         ji.setFill( WHITE );
         ji.setStrokeType( StrokeType.OUTSIDE );
         ji.setStroke(Color.web( "black",0.70 ));
         ji.setStrokeWidth(5);
         Text xian= new Text( "限" );
-        xian.setX( 440 );
-        xian.setY( 400 );
+        xian.setX( 495 );
+        xian.setY( 455 );
         xian.setFont( Font.font("汉仪雪君体繁", FontWeight.BOLD,80 ));
         xian.setFill( WHITE );
         xian.setStrokeType( StrokeType.OUTSIDE );
         xian.setStroke(Color.web( "black",0.70 ));
         xian.setStrokeWidth(5);
 
+
         gc.setFill(WHITE);
-        gc.fillArc(115,100,300,300,90,180, ArcType.OPEN);
+        gc.fillArc( 170, 155, 300, 300, 90, 180, ArcType.OPEN );
 
         gc.setFill(BLACK);
-        gc.fillArc(115, 100, 300, 300, 270, 180, ArcType.OPEN);
+        gc.fillArc( 170, 155, 300, 300, 270, 180, ArcType.OPEN );
 
-        gc.fillArc(190,250,150,150,90,180,ArcType.OPEN);
+        gc.fillArc( 245, 305, 150, 150, 90, 180, ArcType.OPEN );
 
         gc.setFill(WHITE);
-        gc.fillArc(190, 100, 150, 150, 270, 180, ArcType.OPEN);
+        gc.fillArc( 245, 155, 150, 150, 270, 180, ArcType.OPEN );
         gc.setFill(Color.web( "white",0.95 ));
 
 
-        gc.fillOval(250,300,40,40);
+        gc.fillOval( 305, 355, 40, 40 );
         gc.setFill(BLACK);
-        gc.fillOval(250, 150, 40, 40);
+        gc.fillOval( 305, 205, 40, 40 );
 
+        root.getChildren().add( blackRec );
+        root.getChildren().add( whiteRec );
         root.getChildren().add(colors);
         root.getChildren().addAll( circle, yin,yang,liang,yi,dao,wu,ji,xian );
 
@@ -161,8 +180,5 @@ public class yinyangtaiji extends Application {
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
-    }
-    public static void main(String[] args) {
-        launch();
     }
 }
